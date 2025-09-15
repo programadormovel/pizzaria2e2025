616bea958b89128ead2ee89e34aa8a0883212dec
Adriano Domingues <54463264+programadormovel@users.noreply.github.com>
2025-09-15 10:55:27 +0000

    listagem de produtos

---
+++ src/pages/produtos/index.jsx
@@ -0,0 +1,17 @@
+const Produtos = () => {
+    
+    
+    return(
+    <>
+        <h3>Listagem de Produtos</h3>
+        <ul>
+            <li>Pizza de Muçarela</li>
+            <li>Pizza de Calabresa</li>
+            <li>Pizza Baiana</li>
+            <li>Pizza Portuguesa</li>
+        </ul>
+    </>
+    )
+}
+
+export default Produtos
