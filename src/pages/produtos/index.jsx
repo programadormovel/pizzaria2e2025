9d8d55d8fbd056f7726ee3f99ae623f58d96700f
Adriano Domingues <54463264+programadormovel@users.noreply.github.com>
2025-09-15 12:12:51 +0000

    consumo ajustado

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,17 +1,37 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react';
+
 const Produtos = () => {
-    
-    
+    const [dados,setDados] = useState([]);
+    // Consumir os produtos do endpoint (rota)
+    const apiUrl = 'http://172.19.0.49/pizzariaoficial/api/v1'
+    const api = axios.create({baseURL: apiUrl})
+    const pegarProdutos = async () => {
+        await api.get("/produto")
+        .then((response)=>{
+            setDados(response.data);
+        }).catch(err => {
+            console.error("Erro na requisição:", err.message);
+            console.error("Config:", err.config?.url);
+        });   
+    }
+    useEffect (() => {
+        pegarProdutos()
+    }, []);
+    useEffect(()=>{
+        alert(JSON.stringify(dados))
+    }, [dados])
+
+    // Mapeamento das pizzas da lista (iteração)
+    const listaPizzas = dados.map(pizza => 
+        <li key={pizza.id}>{pizza.nome}</li>);
     return(
     <>
         <h3>Listagem de Produtos</h3>
         <ul>
-            <li>Pizza de Muçarela</li>
-            <li>Pizza de Calabresa</li>
-            <li>Pizza Baiana</li>
-            <li>Pizza Portuguesa</li>
+            {listaPizzas}
         </ul>
     </>
     )
 }
-
 export default Produtos
